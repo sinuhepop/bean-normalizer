9824917eb5d7f59a0be4985fdc8db68972cba15a
sinuhepop <sinuhepop@gmail.com>
2018-12-20 01:44:27 +0100

    z

--- src/main/java/tk/spop/normalization/core/impl/ReflectionClassPropertyRegistry.java
+++ src/main/java/tk/spop/normalization/core/impl/ReflectionClassPropertyRegistry.java
@@ -1,5 +1,6 @@
 package tk.spop.normalization.core.impl;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,10 +11,22 @@ import tk.spop.normalization.core.TransformationFinder;
 public class ReflectionClassPropertyRegistry implements TransformationFinder {
 
 	private final Map<Class<?>, Transformation> map = new ConcurrentHashMap<>();
+	private final Map<Class<? extends Annotation>, Object> annotations = new ConcurrentHashMap();
 
 	@Override
 	public List<Transformation> findTransformations(Class<?> type) {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	private List<Transformation> findFields(Class<?> type) {
+
+//		Arrays.stream(type.getDeclaredFields()) //
+//		.map(FieldAccessor::new) //
+//		.map(mapper)
+
+		return null;
+	}
+
 }
